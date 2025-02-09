f06632ec490d48d923d8a88482253e3cdeeb55a9
lydiati21 <lydiatilahun12@gmail.com>
2025-02-10 00:14:53 +0300

    corrected server.js

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const userRouter = require("./server/api/users/user.router");
  const questionRouter= require("./server/api/Question/question.router")
 const answerRouter = require("./server/api/answer/answer.router");
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT;
 
 const app = express();
 
