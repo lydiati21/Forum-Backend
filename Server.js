8ff632e838390b9b901486615e36e394979a07a4
lydiati21 <lydiatilahun12@gmail.com>
2025-02-10 16:56:54 +0300

    Fix: Ensure server listens to process.env.PORT

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const userRouter = require("./server/api/users/user.router");
  const questionRouter= require("./server/api/Question/question.router")
 const answerRouter = require("./server/api/answer/answer.router");
 
-const port = process.env.DB_HOST;
+const port = process.env.DB_HOST || 4000;
 
 const app = express()
 
